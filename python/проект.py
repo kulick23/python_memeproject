3aa1b30377a9d763b944ff09e88885b4b1d2f47d
Danila Kulinkovich <81530683+kulick23@users.noreply.github.com>
2022-07-16 15:06:58 +0300

    v1

---
+++ python/проект.py
@@ -0,0 +1,381 @@
+from tkinter import*
+def Uganda():
+    w=Toplevel(n)
+    w['bg']='purple'
+    global po
+    po=PhotoImage(file=('1.gif'),width=682,height=431)
+    lo=Button(w,image=po)
+    
+    
+    m1=Label(w,text='Кто такой кнаклс?',font='Arial 14',bg='purple')
+    m2=Label(w,text='Ugandan Knuckles — пародийное изображение персонажа Наклса игровой франшизы \n\
+Sonic, который выглядит как маленький красный круглый зверек с руками и ногами.\n\
+\n\
+ Его часто используют в качестве аватара игроки многопользовательской игры VRChat.',bg='purple')
+             
+   
+    
+    m1.pack()
+    m2.pack()                                                                           
+    lo.pack()
+    
+    
+
+def Uganda2():
+    w=Toplevel(n)
+    w['bg']='purple'
+    global po
+    po=PhotoImage(file=('2.gif'),width=320,height=180)
+    lo=Button(w,image=po)
+
+    m1=Label(w,text='история происхождения',font='Arial 14',bg='purple')                 
+
+    m8=Label(w,text='Безобразный Наклс появился в видео всего на несколько секунд, но почему-то полюбился пользователям. \n\
+\n\
+Через две недели Gregzilla выложил короткое видео, в котором его персонаж «поет» под песню I Don’t Want to Set The World On Fire.\n\
+\n\
+Таким образом он положил начало флешмобу Knuckles sings, участники которого делали пародии и заставляли Наклса подпевать разным мелодиям.\n\
+\n\
+ВВ сентябре появилась 3D-версия круглого Наклса, которую сделал пользователь DeviantArt tidiestflyer — эта модель и стала основой для последующих мемов про Ugandan Knuckles.\n\
+\n\
+PЗимой это изображение стали массово использовать в качестве аватара в многопользовательской игре VRChat .\n\
+\n\
+В цТогда же и сформировалось прозвище персонажа — Ugandan Knuckles. Оно связано с локальными мемами про Воинов Уганды, которые форсили игроки PlayerUnknown’s Battlegrounds и фанаты стримера Forsen.',bg='pink')
+    m1.pack()                                                                                                                                                                                           
+    m8.pack()
+    
+    lo.pack()                                                            
+def Имоемусынутоже():
+    w=Toplevel(n)
+    w['bg']='purple'
+    global po
+    po=PhotoImage(file=('3.gif'),width=406,height=250)
+    lo=Button(w,image=po)
+    
+    m3=Label(w,text='И моему сыну тоже',font='Arial 14',bg='purple')
+    m4=Label(w,text='И моему сыну тоже — мем, на котором изображены два предмета с лицами «ржумена»\n\
+    \n\
+    (смайлы в очках, показывающие рукой жест «класс»). Первый персонаж, самый большой,\n\
+    \n\
+    просит что-то для себя и для своего сына (маленький смайл).',bg='purple')
+    
+    m5=Label(w,text='история происхождения',font='Arial 14',bg='purple')
+    m6=Label(w,text='Впервые в рунете мемы с желтыми круглыми человечками стали делать в паблике «ржу» во «ВКонтакте»,\n\
+    \n\
+    который был создан в начале 2015 года.\n\
+    Об этом рассказал бывший админ сообщества Данил Гордин.\n\
+    В основе шаблона лежит смайл в солнцезащитных очках, который одной рукой показывает «класс», а другой — держит дужку очков, как будто хочет их снять. \n\
+    Такого персонажа стали называть «ржумен» — он стал своего рода символом сообщества и появлялся во многих мемах.',bg='purple')
+    m3.pack()
+    m4.pack()
+    m5.pack()
+    m6.pack()
+    
+    lo.pack()
+def Имоемусынутоже2():
+    w=Toplevel(n)
+    w['bg']='purple'
+    global po
+    po=PhotoImage(file=('4.gif'),width=1280,height=704)
+    lo=Button(w,image=po)
+    
+    m3=Label(w,text='Значение',font='Arial 14',bg='purple')
+    m4=Label(w,text='Мем «И моему сыну тоже» — по сути серия фотошоп-пародий оригинального мема с «ржуменами».\n\
+    Первоначальный вариант не нес какой-либо смысловой нагрузки, а был создан ради комического эффекта.\n\
+    Другие картинки, на которых вместо смайлов изображены парные предметы, просто обыгрывают оригинальную шутку.\n\
+    Многие из них содержат иронию, связанную с очеловеченным предметом.\n\
+    Например, мем с логотипами браузера Chrome, которые просят побольше «оперативной памяти».',bg='purple')
+   
+
+    m3.pack()
+    m4.pack()
+    lo.pack()
+    
+def mem3():
+    w=Toplevel(n)
+    w['bg']='purple'
+    global po
+    po=PhotoImage(file=('5.gif'),width=767,height=574)
+    lo=Button(w,image=po)
+    m2=Label(w,text='Папа отключает сына-аутиста от аппарата жизнеобеспечения',font='Arial 14',bg='purple')
+    
+    m7=Label(w,text='Папа отключает сына-аутиста от аппарата жизнеобеспечения — комикс с мальчиком,\n\
+    \n\
+    который говорит какую-то чушь, лежа на больничной койке, и отцом, который после этого отключает сына от аппарата жизнеобеспечения.\n\
+    На оригинальном комиксе есть первая панель с врачом, которая говорит, что у мальчика аутизм, но как мем чаще всего используются две последние панели.',bg='purple')
+    
+   
+
+
+    m2.pack()
+    m7.pack()
+    lo.pack()
+
+def mem32():
+    w=Toplevel(n)
+    w['bg']='purple'
+    global po
+    po=PhotoImage(file=('6.gif'),width=1659,height=800)
+    lo=Button(w,image=po)
+    
+    m2=Label(w,text='происхождение',font='Arial 14',bg='purple')
+    m3=Label(w,text='Известный в сети художник Shadman (настоящее имя — Shaddai Prejean) 10 января 2018 года опубликовал комикс с сюжетом из больницы.\n\
+    На первой панели врач говорит отцу, что у его сына аутичный фиброз и болезнь медленно убивает мальчика.\n\
+    Врач предлагает принять тяжелое решение и выбрать — отпустить ребенка или дальше смотреть, как он страдает.\n\
+    На второй панели мальчик просыпается и отец спрашивает у него, как он себя чувствует, а мальчик говорит: «DO  YOU NOT KNOW DA WAE?!»,\n\
+    \n\
+    что отсылает к мему Ugandan Knuckles, который стал очень популярным и начал раздражать некоторых.\n\
+    В итоге отец со слезами на глазах отключает аппарат жизнеобеспечения из розетки.',bg='purple')                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                      
+    m2.pack()
+    m3.pack()
+    lo.pack()
+
+def Лохцветочный():
+    w=Toplevel(n)
+    w['bg']='purple'
+    global po
+    po=PhotoImage(file=('7.gif'),width=480,height=315)
+    lo=Button(w,image=po)
+    
+  
+    m3=Label(w,text='Лох цветочный',font='Arial 14',bg='purple')
+    m4=Label(w,text='Лох цветочный — видео про бородатого мужчину с букетом, который подошел на улице к человеку, снимающему драку на телефон, и попросил прекратить запись.\n\
+    За это был послан автором видео целым потоком матерных фраз, самые яркая из которых «лох цветочный».\n\
+    Действие на видео, предположительно, происходит в Краснодаре.',bg='purple')
+    
+ 
+ 
+    m3.pack()
+    m4.pack()
+    lo.pack()
+
+def Лохцветочный2():
+    w=Toplevel(n)
+    w['bg']='purple'
+    global po
+    po=PhotoImage(file=('8.gif'),width=480,height=314)
+    lo=Button(w,image=po)
+    
+   
+    m3=Label(w,text='Происхождение',font='Arial 14',bg='purple')
+    m4=Label(w,text='Видео с конфликтом мужчин на улице стало вирусно распространяться 13 января 2018 года: его опубликовали на сайте «ЯПлакал» и «Дваче», много копий распространилось на Youtube.\n\
+    Ролик начинается с того, что двое мужчин начинают драться рядом с овощным ларьком.\n\
+    В этот момент к снимающему подходит бородатый мужчина с букетом и призывает прекратить съемку.\n\
+    Автор видео начинает его оскорблять, видео заканчивается его репликой: «Иди отсюда, лох цветочный!»',bg='purple')
+    m3.pack()
+    m4.pack()
+    lo.pack()
+def Лохцветочный3():
+    w=Toplevel(n)
+    w['bg']='purple'
+    global po
+    po=PhotoImage(file=('9.gif'),width=1280,height=891)
+    lo=Button(w,image=po)
+    
+
+    m3=Label(w,text='Значение',font='Arial 14',bg='purple')
+    m4=Label(w,text='Видео стало популярным, прежде всего, из-за внезапности конфликта: мужчина с бородой и букетом неожиданно подошел и начал просить прекратить съемку, хотя не имел отношения к драке.\n\
+    Мнение пользователей сети разделилось: одни считают, что мужчина зря вмешался, другие осуждают автора видео, который достал телефон и начал оскорблять незнакомого человека.',bg='purple')
+    m3.pack()
+    m4.pack()
+    lo.pack()
+def Очкивиртуальнойреальности():
+    w=Toplevel(n)
+    w['bg']='purple'
+    global po
+    po=PhotoImage(file=('10.gif'),width=731,height=746)
+    lo=Button(w,image=po)
+    
+    m3=Label(w,text='Очки виртуальной реальности',font='Arial 14',bg='purple')
+    m4=Label(w,text='Очки виртуальной реальности (Все так реалистично) — комикс с человечком, который надевает VR-очки и говорит, как реалистично все выглядит.\n\
+    На следующей панели пишут какую-либо отрезвляющую и неприятную информацию о жизни, а в конце человек плачет. \n\
+    Мем строится на том, что очки должны были бы показывать красивую реалистичную картинку, но показывают жизненные реалии.',bg='purple')
+    m3.pack()
+    m4.pack()
+    lo.pack()
+
+def Очкивиртуальнойреальности2():
+    w=Toplevel(n)
+    w['bg']='purple'
+    global po
+    po=PhotoImage(file=('11.gif'),width=500,height=500)
+    lo=Button(w,image=po)
+   
+    m3=Label(w,text='происхождение',font='Arial 14',bg='purple')
+    m4=Label(w,text='Художник комиксов с псевдонимом Lemon Sandwich (Лимонный Сендвич) 7 августа 2017 \n\
+    года опубликовал в фейсбуке комикс, в котором человек надевает очки виртуальной \n\
+   реальности и говорит: «Как все реалистично!» Затем он видит баланс своего банковского счета, на котором минус 1352 фунта стерлингов, и начинает плакать.',bg='purple')
+    m3.pack()
+    m4.pack()
+
+    lo.pack()
+def test():
+    
+    def otvet():
+        n=0
+        if var.get()==1:
+            n=n+1
+        if var1.get()==2:
+            n=n+1
+        if var10.get()==1 and var20.get()==1 and var40.get()==1:
+            n=n+1                   
+        if var3.get()==1:
+            n=n+1
+        if var100.get()==1 and var200.get()==1:
+            n=n+1
+        t1.delete(0,END)
+        t1.insert(0,n)
+    w=Toplevel(n)
+    m1=Label(w,text='Тест по мемознанию:',font='Arial 14')
+    m1.pack()
+    m1=Label(w,text='Каждый правильный ответ будет защитываться по 1 баллу',font='Arial 11')
+    m1.pack()
+    m2=Label(w,text='1.из какой игры кнаклс',font='Arial 11')
+    m2.pack()
+    
+    var=IntVar()
+    var.set(0)
+    
+    rb1=Radiobutton(w,text='vr chat',variable=var,value=1)
+    rb2=Radiobutton(w,text='pubg',variable=var,value=2)
+    rb3=Radiobutton(w,text='mortal combat x',variable=var,value=3)
+    rb1.pack()
+    rb2.pack()
+    rb3.pack()
+    m3=Label(w,text='2.Как называется главный герой мема "и моему сыну тоже"?',font='Arial 11')
+    m3.pack()
+     
+    var1=IntVar()
+    var1.set(0)
+    
+    rb1=Radiobutton(w,text='Uganda warior',variable=var1,value=1)
+    rb2=Radiobutton(w,text='ржумен',variable=var1,value=2)
+    rb3=Radiobutton(w,text='дратути',variable=var1,value=3)
+    rb1.pack()
+    rb2.pack()
+    rb3.pack()
+
+    m6=Label(w,text='3.выберите правельный ответ',font='Arial 11')
+    m6.pack()
+    var10=IntVar()
+    var20=IntVar()
+    var30=IntVar()
+    var40=IntVar()
+
+    var10.set(0)
+    var20.set(0)
+    var30.set(0)
+    var40.set(0)
+
+    ch1=Checkbutton(w,text='кнаклс из уганды',variable=var10,onvalue=1,offvalue=0)
+    ch2=Checkbutton(w,text='кнаклс спрашивает "знаешь ли ты путь?"',variable=var20,onvalue=1,offvalue=0)
+    ch3=Checkbutton(w,text='кнаклс спрашивает "знаешь ли ты где евроопт?"',variable=var30,onvalue=1,offvalue=0)
+    ch4=Checkbutton(w,text='ездил ли он на танке?',variable=var40,onvalue=1,offvalue=0)
+    ch1.pack()
+    ch2.pack()
+    ch3.pack()
+    ch4.pack()
+
+    m2=Label(w,text='4.Зачем отец оключил аппарат сыну',font='Arial 11')
+    m2.pack()
+    
+    var3=IntVar()
+    var3.set(0)
+    
+    rb1=Radiobutton(w,text='из-за болезни',variable=var3,value=1)
+    rb2=Radiobutton(w,text='просто так',variable=var3,value=2)
+    rb3=Radiobutton(w,text='по приколу',variable=var3,value=3)
+    rb1.pack()
+    rb2.pack()
+    rb3.pack()
+
+    
+    m7=Label(w,text='5.выбирете правильный ответ',font='Arial 11')
+    m7.pack()
+    var100=IntVar()
+    var200=IntVar()
+    var300=IntVar()
+
+    var100.set(0)
+    var200.set(0)
+    var300.set(0)
+
+    ch1=Checkbutton(w,text='жирный',variable=var100,onvalue=1,offvalue=0)
+    ch2=Checkbutton(w,text='лох цветочный',variable=var200,onvalue=1,offvalue=0)
+    ch3=Checkbutton(w,text='лох фруктовый',variable=var300,onvalue=1,offvalue=0)
+    ch1.pack()
+    ch2.pack()
+    ch3.pack()
+
+    
+    b1=Button(w,text='Ваше количество баллов:',font='Arial 13',command=otvet)
+    b1.pack()
+  
+
+    t1=Entry(w,width=5)
+    t1.pack()
+
+    
+    
+n=Tk()
+n.geometry('600x400')
+n['bg']='purple'
+
+global po
+po=PhotoImage(file=('12.gif'),width=800,height=450)
+lo=Button(n,image=po)
+
+m7=Label(n,text='Мем',font='Arial 14',bg='purple')
+m5=Label(n,text='Мем (англ. meme) — единица культурной информации.\n\
+Мемом может считаться любая идея, символ, манера или образ действия, осознанно или неосознанно передаваемые от человека к человеку посредством речи, письма, видео, ритуалов, жестов и т. д.\n\
+Концепция мема и сам термин были предложены эволюционным биологом Ричардом Докинзом в 1976 году в книге «Эгоистичный ген».\n\
+Докинз предложил идею о том, что вся культурная информация состоит из базовых единиц — мемов, точно так же как биологическая информация состоит из генов; и так же как гены, мемы подвержены естественному отбору, мутации и искусственной селекции.\n\
+ На основе этой идеи Докинза возникла дисциплина меметика, в настоящее время имеющая спорный научный статус',bg='purple')
+m7.pack()
+m5.pack()
+lo.pack()
+
+
+
+a=Menu()
+b=Menu()
+v=Menu()
+e=Menu()
+r=Menu()
+p=Menu()
+u=Menu()
+v.add_command(label='Кто такой кнаклс?',command=Uganda)
+v.add_command(label='история происхождения',command=Uganda2)
+
+b.add_command(label='И моему сыну тоже',command=Имоемусынутоже)
+b.add_command(label='Значение',command=Имоемусынутоже2)
+
+
+e.add_command(label='Папа отключает сына-аутиста от аппарата жизнеобеспечения',command=mem3)
+e.add_command(label='происхождение',command=mem32)
+
+
+r.add_command(label='Лох цветочный',command=Лохцветочный)
+r.add_command(label='Происхождение',command=Лохцветочный2)
+r.add_command(label='Значение',command=Лохцветочный3)
+
+p.add_command(label='Очки виртуальной реальности',command=Очкивиртуальнойреальности)
+p.add_command(label='происхождение',command=Очкивиртуальнойреальности2)
+
+u.add_command(label='Тест по мемознанию',command=test)
+
+
+
+
+
+a.add_cascade(label='Uganda warior       ',menu=v)
+a.add_cascade(label='И моему сыну тоже      ',menu=b)
+a.add_cascade(label='Папа отключает сына-аутиста от аппарата жизнеобеспечения      ',menu=e)
+a.add_cascade(label='Лох цветочный        ',menu=r)
+a.add_cascade(label='Очки виртуальной реальности      ',menu=p)
+a.add_cascade(label='Тест по мемознанию ',menu=u)
+
+
+n.config(menu=a)
+
+
